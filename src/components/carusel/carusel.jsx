fbfbfff3bd8bceb7f32b87264d5ff24704716675
anhna <rsg.ana8@gmail.com>
2025-10-14 18:20:39 -0600

    change sizing

--- src/components/carusel/carusel.jsx
+++ src/components/carusel/carusel.jsx
@@ -6,8 +6,10 @@ const carusel = () => {
     <div className='p-8 text-center'>
         <h2 className='text-2xl font-bold text-[#333333]'>Inspiration Collection</h2><br />
         <p className='text-sm text-[#666666]'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-        <div className='p-20'>
-            <a href="#"><img src={Carrusel} alt="collection images" className='w-full'/></a>
+        <div className="flex justify-center items-center p-1 h-[450px] w-full">
+          <a href="#">
+            <img src={Carrusel} alt="collection images" className="w-[50rem]" />
+          </a>
         </div>
     </div>
   )
