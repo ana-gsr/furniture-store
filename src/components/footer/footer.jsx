aa8a353de8e221544b1a9618482115005f70dcc9
anhna <rsg.ana8@gmail.com>
2026-02-11 18:58:19 -0600

    add images to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,3 +1,8 @@
+import IG1 from '../../assets/IG-1.jpg' 
+import IG2 from '../../assets/IG-2.jpg'
+import IG3 from '../../assets/IG-3.jpg'
+import IG4 from '../../assets/IG-4.jpg'
+
 const Footer = () => {
   return (
     <div className="px-8 py-20 bg-[#054C73] flex flex-col md:flex-row justify-between items-start gap-8">
@@ -12,10 +17,11 @@ const Footer = () => {
       <div className="md:w-1/2 text-center">
         <h3 className="text-xl font-bold text-white mb-6">Instagram Shop</h3>
         <div className="grid grid-cols-2 gap-4">
-          <img src="" alt="Decoración" className="w-full h-auto rounded" />
-          <img src="" alt="Cesta con oso" className="w-full h-auto rounded" />
-          <img src="" alt="Macramé" className="w-full h-auto rounded" />
-          <img src="" alt="Cocina moderna" className="w-full h-auto rounded" />
+          <img src={IG1} alt="Decoración" className="w-full h-auto rounded" />
+          <img src={IG2} alt="Cesta con oso" className="w-full h-auto rounded" />
+          <img src={IG3} alt="Macramé" className="w-full h-auto rounded" />
+          <img src={IG4} alt="Cocina moderna" className="w-full h-auto rounded" />
+
         </div>
       </div>
     </div>
